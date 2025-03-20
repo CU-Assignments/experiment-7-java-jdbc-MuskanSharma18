18b50e42ee8745cdc13e904354d06d373252304e
MuskaanVishamber <133594818+MuskanSharma18@users.noreply.github.com>
2025-03-20 12:30:35 +0530

    Create EmployeeFetcher.java

---
+++ EmployeeFetcher.java
@@ -0,0 +1,25 @@
+import java.sql.*;
+
+public class EmployeeFetcher {
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/your_database";
+        String user = "your_username";
+        String password = "your_password";
+
+        String query = "SELECT EmpID, Name, Salary FROM Employee";
+
+        try (Connection con = DriverManager.getConnection(url, user, password);
+             Statement stmt = con.createStatement();
+             ResultSet rs = stmt.executeQuery(query)) {
+
+            while (rs.next()) {
+                int empID = rs.getInt("EmpID");
+                String name = rs.getString("Name");
+                double salary = rs.getDouble("Salary");
+                System.out.println("EmpID: " + empID + ", Name: " + name + ", Salary: " + salary);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
